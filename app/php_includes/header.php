7d1ffb29e7db1164d3f54d026ab970785335f53a
BBrownley <brendobrownley@yahoo.com>
2019-01-17 16:12:45 -0500

    tested gulp-connect-php plugin

---
+++ app/php_includes/header.php
@@ -0,0 +1,3 @@
+<section class="section-name">
+  <h4>This is a section</h4>
+</section>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My site</title>
+
+	<?php
+		include_once("./app/php_includes/header.php");
+	?>
+
+  <h1>Hello</h1>
+
+</head>
+<body>
+
+</body>
+
+</html>
